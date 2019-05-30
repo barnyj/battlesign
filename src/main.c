75ce8b7f460041a81658bb51f1c071b8fe1445c8
Barnaba Piotrowski <barnaba.piotrowski@gmail.com>
2019-05-30 23:30:40 +0200

    first commit

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "sds.h"
+char *_UNDEFINED_BS_CHARACTER = "!^UNDEFINED^!";
+
+typedef struct GAMESTATE {
+  char *player;
+  char *enemy;
+} bs_gamestate;
+
+bs_gamestate init_gamestate() {
+  bs_gamestate new_gamestate = {_UNDEFINED_BS_CHARACTER, _UNDEFINED_BS_CHARACTER};
+  return new_gamestate;
+}
+
+void start_message() {
+  puts("Welcome to battlesign.");
+}
+
+void end_message() {
+  puts("Finished. Exiting battlesign.");
+}
+
+void game_init(bs_gamestate *gamestate) {
+  puts("Type name of player:");
+  char buf[100];
+  fgets(buf, 100, stdin);
+  printf("Name of player: %s\n", buf);
+  gamestate->player = buf;
+}
+
+int main() {
+  start_message();
+  bs_gamestate gamestate = init_gamestate();
+  game_init(&gamestate);
+  end_message();
+  return 0;
+}
+
